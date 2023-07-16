6cbe1e9904b4fa31cf17e5a58971ffe7d7fe4a79
Jeka_A <antimur4ik@yandex.ru>
2023-07-16 19:24:27 +0300

    Найти среднее арифметическое двух чисел

---
+++ Урок #8/Task_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти среднее арифметическое двух чисел
+
+double firstValue, secondValue;
+Console.WriteLine("Введите число 1");
+firstValue = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+secondValue = double.Parse(Console.ReadLine());
+double result = (firstValue + secondValue) / 2;
+Console.WriteLine("Cреднее арифметическое двух чисел = " + result);
+
+
