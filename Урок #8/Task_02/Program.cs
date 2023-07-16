175fc4225dcc3661aaaf55f1436079cbb915bb3f
Jeka_A <antimur4ik@yandex.ru>
2023-07-16 20:09:18 +0300

    Сумма и произведения трех чисел

---
+++ Урок #8/Task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Введите три числа и выведите на экран значение суммы и произведения этих чисел.
+
+double firstValue, secondValue, thirdValue;
+Console.WriteLine("Введите число 1");
+firstValue = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+secondValue = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+thirdValue = double.Parse(Console.ReadLine());
+double sumResult = (firstValue + secondValue + thirdValue);
+double multResult = firstValue * secondValue * thirdValue;
+Console.WriteLine("Cумма трех чисел = " + sumResult);
+Console.WriteLine("Произведение трех чисел = " + multResult);
